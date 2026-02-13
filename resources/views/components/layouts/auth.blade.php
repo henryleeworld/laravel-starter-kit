4917ae230bcfa41bbcedd3e0e76820b7a78efee7
Henry Lee <henryleeworld@gmail.com>
2026-02-14 00:13:40 +0800

    feat:更新最新資料

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - {{ config('app.name') }}</title>
+    <title>{{ config('app.name') }}</title>
     @vite('resources/css/app.css')
 </head>
 
